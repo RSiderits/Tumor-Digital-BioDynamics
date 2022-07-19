6500668e6c13db77add988dbd83a62c9ea7e81e9
RSiderits <109485517+RSiderits@users.noreply.github.com>
2022-07-19 17:51:18 -0400

    Version 1.1
    Several bug fixes

--- TCC_GUI.py
+++ TCC_GUI.py
@@ -2,6 +2,21 @@
 
 # Author Richard Siderits July 17 2022
 
+# Version 1.1 
+
+# Bug fixes:
+    
+    # - Crash when a data entry field left blank in "calc"
+    # - Check cell counts go the expected direction with TIS Changes
+    # - Added "Press Tab key to advance.
+    # - Kept commented out print statements at end
+    
+ # Consider:
+     
+     # - Write data and Sampel ID to a Word file
+     # - Write print sttaments to a logger
+     # - Add a Compenstaiton Slider to match predicted result extrction method
+
 # Import packages
 
 import PySimpleGUI as sg   
@@ -12,17 +27,19 @@ import math
 
 def check():
           
+      if event =="-ID-" :
       
-          window["-TS-"].update("1") 
+          if values["-TS-"]== "":
           
+           window["-TS-"].update("1") 
               
-          window["-CS-"].update("1")            
-      
+           window["-CS-"].update("10")            
           
-          window["-PS-"].update("1") 
+           window["-PS-"].update("10") 
           
-              
-          window["-NT-"].update("1")     
+           window["-NT-"].update("10") 
+          
+           window["-EE-"].update("80")    
          
           return 
 
@@ -32,7 +49,8 @@ def check():
     
     # Nuclear DNA Content Varies with Cell Size across Human Cell Types
           
-    # Non-tumor cellularity is epithelial, mesenchymal, immune, endothelial, fribrblast)
+    # Non-tumor cellularity - epithelial, mesenchymal, immune, endothelial, 
+    #fribrblast)
     
     # TIS = Tumor Induced Stroma (Tumor microenvironment)
     
@@ -52,8 +70,10 @@ def check():
     # Picograms DNA are from Enhanced for tumor samples (stroma rmoved)
     # Count necrosis as stromal for estimates of non-tumor
     # Average cell has about 6 pg DNA
-    # Non-tumor cellularity is epithelial, mesenchymal, immune, endothelial, fribrblast)
-    # "Enhanced for tumor" follows microdissection, non-tumor stroma is excised from block
+    # Non-tumor cellularity is epithelial, mesenchymal, immune, endothelial, 
+    # fribrblast)
+    # "Enhanced for tumor" follows microdissection, non-tumor stroma is 
+    # excised from block
     # Extraction efficiency and proportion of Tumor Induced Stroma (TIS) vary.
     # Necrosis in Tumor Cell Field (TCF), should be counted as non-tumor.
     
@@ -65,10 +85,12 @@ def check():
     #         - Average size of the tumor cells in um, measure up o 20 cells.
     #         - Percent TIS
     #         - Cellularity of TIS, include areas of necrosis
-    #     3) If you are using a core, then use the core diameter as the leasion size
+    #     3) If you are using a core, then use the core diameter as the leasion 
+    #        size
     #     4) Enter variables into the respective fields then click "calculate"
 
-    # The final field will provide an estmated number of 10um thick secton to assure 100ng of Tumor DNA 
+    # The final field will provide an estmated number of 10um thick secton to 
+    #assure 100ng of Tumor DNA 
     
     
 # --------------------------- Variables ------------------------------------ @
@@ -111,42 +133,49 @@ layout=[
          sg.Text("Sample ID:", border_width=3, background_color="black", 
                  text_color="yellow"), 
         
-         sg.Input(" Default values", size=(15,1), border_width=7, 
-         text_color="yellow", enable_events=True, key="-ID-", 
+         sg.Input(" Start here", size=(15,1), border_width=7, 
+         text_color="yellow", enable_events=True, focus=True, key="-ID-", 
          background_color="black")],
        
         # Get input for parameters: -------------------------------------------
-            
-        [sg.Input(default_text="1", key="-TS-", size=(5,1), text_color="green1", 
+        [sg.Text("Press [Tab] key to advance:", text_color="gold")],
+        [sg.Text()],                    
+        [sg.Input(default_text="1", key="-TS-", size=(5,1), 
+                  text_color="green1", 
                   enable_events=True,tooltip="Enter size of lesion in cm" ),    
         sg.Text("Tumor size in cm (1-10 cm)", size=(25,1))],  
         
-        [sg.Input(default_text="25",key="-CS-", size=(5,1), text_color="green1", 
+        [sg.Input(default_text="25",key="-CS-", size=(5,1), 
+                  text_color="green1", 
                   enable_events=True),
         sg.Text("Average tumor cell size in microns (1-100 um)", size=(35,1))], 
        
-        [sg.Input(default_text="10",key="-PS-", size=(5,1), text_color="green1", 
+        [sg.Input(default_text="10",key="-PS-", size=(5,1), 
+                  text_color="green1", 
                   enable_events=True), 
         sg.Text("Percent stroma (1-99%)", size=(20,1))],  
   
-        [sg.Input(default_text="10", key="-NT-", size=(5,1), text_color="green1", 
+        [sg.Input(default_text="10", key="-NT-", size=(5,1), 
+                  text_color="green1", 
                   enable_events=True),
         sg.Text("Percent stromal cellularity (1-99%)", size=(25,1))],         
    
-        [sg.Input(default_text="85",key="-EE-", size=(5,1), text_color="green1", 
+        [sg.Input(default_text="85",key="-EE-", size=(5,1), 
+                  text_color="green1", 
                   enable_events=True),
         sg.Text("Expected Extraction efficiency (1-99%)", size=(30,1))],        
       
        
-        [sg.Text("----------------------------------------------------------------------")],
+        [sg.Text("---------------------------------------------------------")],
         
         # Clear and quit
         
-        [sg.Button("Calculate", key="-Calc-", pad=(10,25), size=20, font="Arial,12"), 
+        [sg.Button("Calculate", key="-Calc-", pad=(10,25), size=20, 
+                   font="Arial,12"), 
          sg.Button("Clear", font="Arial,12", key="-Clear-", pad=(10,25)),  
          sg.Exit(font="Arial,12", pad=(10,25))],
         
-        [sg.Text("----------------------------------------------------------------------")],
+        [sg.Text("---------------------------------------------------------")],
         # Output fileds for calculations, in order EN,CN,PT -------------------
                         
         [sg.Text("Estimated number of tumor cells:"), 
@@ -158,9 +187,9 @@ layout=[
         [sg.Text("Estimated total picograms tumor DNA:"), 
          sg.Text("0.0", size=(10,1), key="-PT-", text_color="gold")], 
         
-        [sg.Text("----------------------------------------------------------------------")],
+        [sg.Text("---------------------------------------------------------")],
         
-        [sg.Text("Estimated DNA if Enhcanced for Tumor, in a 100um thick setion pg DNA:"), 
+        [sg.Text("Estimated DNA if Enhcanced for Tumor, in a 100um thick section pg DNA:"), 
          sg.Text("0.0", size=(10,1), key="-PDC-", text_color="gold")], 
         
         [sg.Text("STROMAL DNA for 100um thick sample, not enhanced for tumor pg DNA:"), 
@@ -178,7 +207,7 @@ layout=[
         [sg.Text("# of 10um thick sections, enhanced, to assure 100 ng gDNA for sequencing:"), 
          sg.Text("0.0", size=(10,1), key="-sgDNA-", text_color="gold")],
 
-        [sg.Text("----------------------------------------------------------------------")],
+        [sg.Text("---------------------------------------------------------")],
         [sg.Text("")],
         
         # Keys are -TS-, -PS-, -CS-, -CN-, -EN-, -Calc-, -Exit-, -XL-
@@ -195,15 +224,16 @@ window = sg.Window("Estimate Tumor Cell Count and amount tumor DNA", layout)
 while True:
     
     event, values = window.read()
-   
+    
+    
     if event == sg.WIN_CLOSED or event == "Exit":
       break
       
+    
     if event == "-TS-":
-        
+         
          window['-TS-'].update(background_color="navy")
          
-        
     if event == "-CS-":
           
         window['-CS-'].update(background_color="navy")
@@ -218,26 +248,39 @@ while True:
     if event == "-EE-":
                      
        window['-EE-'].update(background_color="navy")
-        
+    
+       
  
     if event == "-Calc-":
               
         v1=values["-TS-"] # Tumor size in cm
-       
+        if values["-TS-"]=="":
+            v1=int(1)
+            window["-TS-"].update(v1)
+            
         v2=values["-PS-"] # Percent stroma (TIS)
-        
+        if values["-PS-"]=="":
+            v2=int(25)
+            window["-PS-"].update(v2)
+            
         v3=values["-CS-"] # Average tumor cell size in microns
-        if v3=="0":
-          v3=int(25)
-          window["-CS-"].update(v3) 
-                
+        if values["-CS-"]=="":
+            v3=int(25)
+            window["-CS-"].update(v3)
+                       
         v4=values["-NT-"] # Percent non-tumor cellularity 
+        if values["-NT-"]=="":
+            v4=int(10)
+            window["-NT-"].update(v3)
+        
         v5=values["-EE-"]
-        
-        # check()
-        
+        if values["-EE-"]=="":
+            v5=int(80)
+            window["-EE-"].update(v3)
+        #check()        
               
-        # window.refresh()
+        #window.refresh()
+       
      
  # ----------- Do the calculations with the input values ----------------------             
         
@@ -278,7 +321,8 @@ while True:
  # Cylinder Volume for 100um high 10, 10um sections:
         VC=int(pi*(tumor_radius**2)*100) # cubic microns
         
- # Number of cells that could be in the sample cylinder, NOT compensated for stroma
+ # Number of cells that could be in the sample cylinder, NOT compensated for 
+ # stroma
         CC=int(VC/cell_volume)
         
  # Stromal DNA Non-Tumor, compensated for %Stroma and Stromal cellularity
@@ -297,12 +341,13 @@ while True:
         
         EECNENG=EECNE/1000
         
- # Number 10um thick sections of enhanced for tumor sample, assure 100ng gDNA for sequencing
+ # Number 10um thick sections of enhanced for tumor sample, assure 100ng gDNA 
+ #  for sequencing
  
         sgDNA=int(round((100/EECNENG)*10,2)) 
 
        
- # ----------  SEND DATA TO Window ----------------------------------------  @   
+ # ----------  SEND DATA TO Window -----------------------------------------  @   
         
         window["-EN-"].update('{:,}'.format(number_cells))  
         window["-CN-"].update('{:,}'.format(comp_number_cells))  
@@ -356,12 +401,14 @@ while True:
 # print("Number of cells in tumor:", '{:,}'.format(number_cells))
 # print("Percentage of tumor that is stroma is:", v2, "%")
 # print("Fraction of tumor that is Tumor cells is:", perstroma)
-# print(f"Number tumor cells Compensated for {v2} % stroma is:", '{:,}'.format(comp_number_cells))
+# print(f"Number tumor cells Compensated for {v2} % stroma is:", '{:,}'
+# .format(comp_number_cells))
 # print("--------------------------------------------------------------")
 # print("Cylinder volume in um3:", '{:,}'.format(VC))
 # print("Cells in Cylinder: ", '{:,}'.format(CC))
 # print("--------------------------------------------------------------")
-# print("Picograms DNA in entire tumor (stroma compensated, 6pg/cell):" ,'{:,}'.format( PT))
+# print("Picograms DNA in entire tumor (stroma compensated, 6pg/cell):" ,
+# '{:,}'.format( PT))
 # print("Picograms of DNA in the sectioned 100um cut", '{:,}'.format(PDC))
 # print("Stromal DNA in pg:", '{:,}'.format(strmlDNA))
 # print("--------------------------------------------------------------")
@@ -374,7 +421,8 @@ while True:
 # print("EEC(% non-tumor cellularity)=", EEC)
 # print("EECNE(% non-tumor cellularity)=", EECNE)
 # print("EECNENG(% non-tumor cellularity)=", EECNENG)
-# print("# of 10um thick, enhanced for Tumor, sections for 1000ng Tumor DNA:", sgDNA)
+# print("# of 10um thick, enhanced for Tumor, sections for 1000ng Tumor 
+# DNA:", sgDNA)
 # print("Sample Id:", )
 window.close()
 
